66245e6341f74b9fb1b737604c9e992a7b1405a5
Houjie80 <86527356+Houjie80@users.noreply.github.com>
2023-04-25 02:31:12 +0700

    Update generator.lua

--- usr/lib/lua/luci/controller/generator.lua
+++ usr/lib/lua/luci/controller/generator.lua
@@ -1,1 +1,9 @@
-sftp://192.168.1.1/usr/lib/lua/luci/controller
+module("luci.controller.generator", package.seeall)
+
+function index()
+
+entry({"admin","services","generator"}, template("generator"), _("Config Generator"), 9999).leaf=true
+
+end
+
+
